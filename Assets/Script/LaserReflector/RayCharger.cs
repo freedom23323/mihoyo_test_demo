155644de0f047a3c7cabaf5f6c6012ee837e5af5
易新志 <1292790915@qq.com>
2026-06-19 16:32:55 +0800

    第三关功能实装

--- Assets/Script/LaserReflector/RayCharger.cs
+++ Assets/Script/LaserReflector/RayCharger.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 public class RayCharger : MonoBehaviour,IHittable
 {
-    [Header("关联的发光控制器")]
+    [Header("关联的发光控制器(不可空)")]
     public GroupGlowController machineGlowManager;
     public GroupGlowController IndicatorLightGlowManager;
-
+    [Header("可选项")]
     public GameObject iceBluePrint;
     public CraftingMachine machine;
     public CoolingTank coolingTank;
+    public GameObject light;
     
     private bool isActive = false;
     // Start is called before the first frame update
     void Start()
     {
         if(iceBluePrint!=null) iceBluePrint.SetActive(false);
+        if (light != null) light.SetActive(false);
     }
 
     // Update is called once per frame
@@ -35,6 +37,7 @@ public class RayCharger : MonoBehaviour,IHittable
         if (coolingTank != null && !coolingTank.isHot) coolingTank.ActivateTankByRayCharger();
         else IndicatorLightGlowManager.SetGlowColor(Color.red);
         if (machine != null) machine.ActiveMachine();
+        if (light != null) light.SetActive(true);
         isActive = true;
     }
 }
